5ac061e7fc131090bb2b4f9ee6a99809011d9952
Scott Rinaberger <scottrinaberger@gmail.com>
2022-12-12 16:06:21 -0600

    add apollo to server.js file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,32 +5,31 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // middleware authentication function
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
+    // verifies JSON Web Token and matches it with data usage
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log("Invalid token");
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
+  // assigns token to a user
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
