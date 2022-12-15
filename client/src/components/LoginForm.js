574f8ab53c25fd708edf1b65f872a7c11cdfe828
Scott Rinaberger <scottrinaberger@gmail.com>
2022-12-15 14:22:27 -0600

    add signupform mutations

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,12 +1,12 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
-import { loginUser } from '../utils/API';
-import Auth from '../utils/auth';
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
